00821fe9d82f2b9d485bed7bb2c668efab95f10b
Bhavuk <bhavukaggarwal789.ba@gmail.com>
2025-03-12 18:12:45 +0530

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Jon Snow",
+                DateOfBirth = new DateTime(2000,11,29)
+            };
+
+            return View(data);
+        }
+    }
+}
